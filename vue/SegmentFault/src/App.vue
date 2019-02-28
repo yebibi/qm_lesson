e3b96f1df208f4529d9c0049c0021c29137d78d7
yebibi <417089207@qq.com>
2019-02-28 20:36:45 +0800

    vue project

---
+++ vue/SegmentFault/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p>message:"{{message}}"</p>
+    <p>Computed reversed message= "{{reversedmessage}}"</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      message:'hello'
+    }
+   
+  },
+  computed:{
+    resvesedmessage:function(){
+       return this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
