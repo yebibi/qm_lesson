bf1c2693da9e64140630d3673fd2eb3640cbbf21
yebibi <417089207@qq.com>
2019-04-01 10:57:59 +0800

    mint-ui引入js组件

--- vue/study/src/App.vue
+++ vue/study/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <mt-button type="primary">primary</mt-button>
+    <mt-button type="primary " plain @click="show">primary</mt-button>
    <h1>这是App组件</h1>
    <router-link to="/account">account</router-link>
   <router-link to="/goodlist">goodlist</router-link>
@@ -9,11 +9,19 @@
 </template>
 
 <script>
-
-
+import {Toast} from 'mint-ui';
 export default {
   name: 'app',
-
+  data(){},
+  methods:{
+    show(){
+      Toast({
+        message:'温馨提示',
+        duration:1000,
+        position:'bottom'
+      })
+    }
+  }
 }
 </script>
 
