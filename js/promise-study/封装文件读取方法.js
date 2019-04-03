c5ea926f8c981356a614b49ddf3674b1cc274819
yebibi <417089207@qq.com>
2019-04-03 12:13:09 +0800

    promise

--- js/promise-study/封装文件读取方法.js
+++ js/promise-study/封装文件读取方法.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-fs.readFile(path.join(__dirname,'./1.txt'),'utf-8',(err,dataStr)=>{
-  if(err) throw err
-  console.log(dataStr)
+// fs.readFile(path.join(__dirname,'./1.txt'),'utf-8',(err,dataStr)=>{
+//   if(err) throw err
+//   console.log(dataStr)
+// })
+function getFileBypath(fpath,callback){
+  fs.readFile(fpath, 'utf-8', (err, dataStr) => {
+    if (err) throw err
+    callback(dataStr)
+  })
+}
+getFileBypath(path.join(__dirname, './1.txt'),(dataStr)=>{
+  console.log(dataStr);
+  
 })
