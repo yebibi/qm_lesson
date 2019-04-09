32ca56c2cebb6cee914180a6e3092b423e0e3798
yebibi <417089207@qq.com>
2019-04-09 10:39:00 +0800

    vue项目之新闻列表

--- vue/study/src/components/news/NewsList.vue
+++ vue/study/src/components/news/NewsList.vue
@@ -1,15 +1,55 @@
 <template>
   <div>
-    新闻资讯
+  <ul class="mui-table-view">
+				<li class="mui-table-view-cell mui-media" v-for="item in newslist" :key="item.id">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" :src="item.img_url">
+						<div class="mui-media-body">
+							<h1>{{item.title}}</h1>
+              <p class="mui-ellipsis">
+                  <span>{{item.add_time}}</span>
+                  <span>{{item.click}}</span>
+              </p>
+						</div>
+					</a>
+				</li>
+			</ul>
   </div>
 </template>
 
 <script>
+import Toast from 'mint-ui';
 export default {
-
+  data(){
+    return {
+      newslist:[] 
+    }
+  },
+  created(){
+    this.getNewsList()
+  },
+  methods:{
+    getNewsList(){
+      this.$http.get('http://www.liulongbin.top:3005/api/getnewslist').then(result=>{
+        if(result.body.status ===0){
+          this.newslist = result.body.message
+        }else{
+          Toast('获取新闻列表失败')
+        }
+      })
+    }
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+.mui-table-view li h1{
+  font-size: 14px;
+}
+.mui-table-view li p{
+  font-size: 12px;
+  color: blue;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
